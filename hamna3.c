ddda5acbe50f3ddc94814d32aeaf77149b8f9a70
hamnaassif-26 <hamnaassif@gmail.com>
2026-09-08 11:36:06 +0500

    Update hamna3.c
    conio header file , getch added

--- hamna3.c
+++ hamna3.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <conio.h>
 int main()
 {
 	int number = 25;
 	char letter = 'R';
 	printf("The number is: %d \n",number);
 	printf("The character is: %c \n \a",letter);
+	getch():
 	return 0;
 }
