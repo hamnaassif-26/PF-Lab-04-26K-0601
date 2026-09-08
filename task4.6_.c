708611be8fb76ae4e7cb08c4708bffec67c13d4f
hamnaassif-26 <hamnaassif@gmail.com>
2026-09-08 08:53:32 -0700

    Update task4.6_.c

--- task4.6_.c
+++ task4.6_.c
@@ -3,23 +3,42 @@
 int main()
 {
 	int month,year;
-	
-	printf("Enter month number (1-12): \n Enter Year: \n");
-	scanf("%d %d",&month,&year);
+	//Array of month names with an empty string at index 0
+	const char *monthNames[] = { "", "January", "Feburary", "March", "April", "May", "June", "July", "August", "Septmeber", "Octuber", "November", "December" };
+
+	printf("Enter month number (1-12): \n");
+	scanf("%d",&month);
+	printf("Enter year: \n");
+	scanf("%d",&year);
 	
 	switch(month)
 	{
-		case '1':
-		case '3':
-		case '5':
-		case '7':
-		case '9':
-		case '11':
-		printf("%s of %d has 31 days",month,year);
-	    break;
-	    case '2': 
-	    printf("")
-	    
-    }
-	
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 12:
+			printf("%s of %d has 31 days",monthNames[month],year);
+			break;
+		
+	       case 4:
+	       case 6:
+	       case 9:
+	       case 11:
+	              printf("%s of %d has 30 days",monthNames[month],year);
+	              break;
+	       case 2:
+	       	if(year%4==0 && year%100!=0 || year%400==0)
+	       	{
+	       		printf("%s of %d has 29 days",monthNames[month],year);
+			}
+			break;
+		default:
+			printf("Error! Invalid month number");
+       }
+	getch();
+	return 0;
 }
+	
+
